14e95ba661af8cfcc6aabcea1536c1b6602a5a5c
MLGrishina <maria107aa@gmail.com>
2025-05-05 15:38:04 +0300

    change files.java

--- src/test/java/homePageObjectsLesson6/TestBase.java
+++ src/test/java/homePageObjectsLesson6/TestBase.java
@@ -7,13 +7,13 @@ import org.junit.jupiter.api.BeforeAll;
 
 public class TestBase {
     @BeforeAll
-    static void beforeAll() {
+    static void installСonfigurations() {
         Configuration.browserSize = "1920x1080"; // разрешение, ну это понятно
         Configuration.pageLoadStrategy = "eager"; // чтоб не ждать загрузки всего сайта , картинок и т.д
         Configuration.baseUrl = "https://demoqa.com"; // выносим абсолютный адресс из опен
     }
     @AfterEach
-    void afterEach() {
+    void afterEachexEcute() {
         Selenide.closeWebDriver();
     }
 
--- src/test/java/pages/components/CalendarComponent.java
+++ src/test/java/pages/components/CalendarComponent.java
@@ -7,9 +7,9 @@ import static com.codeborne.selenide.Selenide.$$;
 public class CalendarComponent {
     public void setData(String day, String month, String year ){
         $("#dateOfBirthInput").click(); //кликаем на поле чтобы активировать календарь
-        $(".react-datepicker__month-select").selectOption("May"); // выбор в календаре мясяца
-        $(".react-datepicker__year-select").selectOption("1900"); //выбор года
-        $$("div.react-datepicker__day").findBy(text("22")).click();// возрат полученной коллекции, ищем в ней 22 и кликаем на число даты, разобрать для понимания
+        $(".react-datepicker__month-select").selectOption(month); // выбор в календаре мясяца
+        $(".react-datepicker__year-select").selectOption(year); //выбор года
+        $$("div.react-datepicker__day:not(.react-datepicker__day--outside-month)").findBy(text(day)).click();
 
 
     }
