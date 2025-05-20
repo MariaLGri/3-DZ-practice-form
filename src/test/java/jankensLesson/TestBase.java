773f420daff97998889c3342686275a1e7c7c9e9
MLGrishina <maria107aa@gmail.com>
2025-05-20 22:33:36 +0300

    change fail .java

---
+++ src/test/java/jankensLesson/TestBase.java
@@ -0,0 +1,41 @@
+package jankensLesson;
+
+import com.codeborne.selenide.Configuration;
+import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import helpers.Attach;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
+import java.util.Map;
+
+public class TestBase {
+    @BeforeAll
+    static void installСonfiguration() {
+        Configuration.browserSize = "1920x1080"; // разрешение, ну это понятно
+        Configuration.pageLoadStrategy = "eager"; // чтоб не ждать загрузки всего сайта , картинок и т.д
+        Configuration.baseUrl = "https://demoqa.com"; // выносим абсолютный адресс из опен
+       Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub"; //хром на стороннем сервисе
+        SelenideLogger.addListener("AllureSelenide", new AllureSelenide());
+
+        DesiredCapabilities capabilities = new DesiredCapabilities();
+        capabilities.setCapability("selenoid:options", Map.<String, Object>of(
+                "enableVNC", true,
+                "enableVideo", true
+        ));
+        Configuration.browserCapabilities = capabilities;
+    }
+    @AfterEach
+    void webDriverTearDown() {
+        Selenide.closeWebDriver();
+    }
+    void addAttachments() {
+        Attach.screenshotAs("Last screenshot");
+        Attach.pageSource();
+        Attach.browserConsoleLogs();
+        Attach.addVideo();
+
+    }
+}
